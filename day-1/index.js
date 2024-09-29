0d8c23111837e85557cce8e0a80cf44d7aacc2c2
withkalim <142097334+withkalim@users.noreply.github.com>
2024-09-29 20:38:37 +0530

    update

---
+++ day-1/index.js
@@ -0,0 +1,38 @@
+// 1.types fo data    
+// String
+// Number
+// Boolean
+// undefined
+// null
+
+// 2. String
+console.log("testing");
+// 3. numbers
+console.log('46');  //number inside the quotes are also comes in string
+console.log(46); //it's true
+// 3. Boolean
+console.log(10 > 20);
+// 4. undefined
+console.log(typeof(car));
+
+//5. operators
+console.log("406" == 406); //its check data same or not
+console.log("406" === 406); //it's check data as well as types
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 >= 10);
+console.log(2 <= 2);
+
+// 6. var variable
+// value assign
+// reassign
+// declare
+
+var number = 4;  // here we assign value
+console.log(number);
+number = 8;      // here we reassign last value to new
+console.log(number);
+var number = 10;  //this assign and declaration
+console.log(number);
+
+
