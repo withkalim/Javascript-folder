d4c403d4ac43bb6456c009fc6e7e43e1ace0d0e1
withkalim <142097334+withkalim@users.noreply.github.com>
2024-09-30 12:17:11 +0530

    update

--- day-2/index.js
+++ day-2/index.js
@@ -1,4 +1,49 @@
-<script>
 
-    
-</script>
+// block scope
+// 1. var variable with scope 
+// {
+//     var x = 100;
+// }
+// console.log(x);
+
+// 2. let variable with ouside of block scope
+
+// {
+//     let  y = "maxx";   
+// }
+// console.log(y);
+// 3. const variable outside the block scope
+
+// {
+//     const z = 15;
+// }
+// console.log(z);
+
+/* take let and cost for assign reassign and declare */
+let number = 10;
+console.log(number);
+number = 100;
+console.log(number);  // here we can see let can reassign
+let number = 130;
+console.log(number);
+
+// let also try it with const variable
+const numbers = 10;
+console.log(numbers);
+numbers = 50;
+console.log(numbers); //here we can see const can't reassign
+const numbers = 90;
+console.log(numbers);
+
+
+// CONDITIONAL STATEMENT
+var number1  = 50;
+var number2 = 100;
+
+if(number < number2){
+    console.log(number + " Is less than " + number2);
+}else if(number == number2){
+    console.log("Both value is qual");
+}else {
+    console.log(number + " Is greater then " + number2);
+}
