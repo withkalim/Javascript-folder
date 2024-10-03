915160d4205ca7d5e1a9a16de670841d6d2a900d
withkalim <142097334+withkalim@users.noreply.github.com>
2024-10-03 22:07:14 +0530

    update

---
+++ day-4/evenOdd.js
@@ -0,0 +1,14 @@
+// Here we Check even & odd number 
+//inside the function.
+
+var number = 3;
+
+function evenOddNumber(number) {
+
+    if (number % 2 === 0) {
+        console.log(number + " Is even number");
+    } else {
+        console.log(number + " Is odd number");
+    }
+}
+evenOddNumber(number);
---
+++ day-4/index.js
@@ -0,0 +1,3 @@
+// A function provide funcianality 
+// We can write multiple line code inside the function and return it
+
