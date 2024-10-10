bb81f2102c7112a54df96f0bf7b5adc229eb8dc5
withkalim <142097334+withkalim@users.noreply.github.com>
2024-10-10 12:40:11 +0530

    update

---
+++ day-6/index.js
@@ -0,0 +1,83 @@
+// Here we will solve broblem with loop
+
+for(var i=1; i < 11; i++){
+    console.log(i);
+}
+
+// iteration 1
+// i = 1;
+// i < 11 is true
+// console.log(1)
+// i++ => Incriment the value by 1
+// i = 2;
+
+// iteration 2
+// i = 2;
+// i < 11 is true  
+// console.log(2)
+// i++ => Incriment by 1
+// i =3;
+
+
+// iteration 3
+// i =3;
+// i < 11 is true
+// console.log(3);
+// i++ => Incriment by 1
+// i = 4;
+
+// iteration 4
+// i =4 ;
+// i < 11 is true
+// console.log(4)
+// i++ => Incriment by 1
+// i = 5;
+
+// iteration 5
+// i = 5;
+// i < 11 is true
+// console.log(5)
+// i++ Incriment by 1
+// i = 6;
+
+// iteration 6
+// i = 6;
+// i < 11 is true
+// console.log(6)
+// i++ Incriment by 1
+// i = 7;
+
+// iteration  7
+// i = 7;
+// i < 11 is true
+// console.log(7)
+// i++ Incriment by 1
+// i = 8;
+
+// iteration 8 
+// i = 8;
+// i < 11 is true
+// console.log(8)
+// i++ Incriment by 1
+// i = 9;
+
+// iteration 9 
+// i = 9;
+// i < 11 is true
+// console.log(9)
+// i++ Incriment by 1
+// i = 10;
+
+// iteration 10 
+// i = 10;
+// i < 11 is true
+// console.log(10)
+// i++ Incriment by 1
+// i = 11;
+
+// iteration 11 most important
+// i = 11; 
+// i < 11 false
+// block will stop to execute
+
+
